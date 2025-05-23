92f7012c25bab80a302c9c5fe308c739e6008a2f
Saksham <sakshamkumar1415@gmail.com>
2025-05-23 11:25:17 +0530

    Fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Components/Home';
+import './styles/app.css'
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <title>123 Fakturera</title>
       <BrowserRouter>
         <Routes>
           <Route path = '/' element = {<Home/>}> </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
